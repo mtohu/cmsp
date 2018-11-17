85ff6460f26a634bfafd86bdee9c0c2da0f50b87
anete <2792688377@qq.com>
2018-11-17 15:57:31 +0800

    change

--- application/wap/controller/Login.php
+++ application/wap/controller/Login.php
@@ -30,11 +30,56 @@ class Login extends Controller {
     }
 
     /**
-     * 检验验证码
+     * 检验登录验证码
      */
-    public function checkVerifyCode()
+    public function checkLoginVerifyCode()
     {
         $this->assign('head_title', '安全检测');
+        return $this->fetch();
+    }
+
+    /**
+     * 重置密码
+     */
+    public function resetPwd()
+    {
+        $this->assign('head_title', '重置密码');
+        return $this->fetch();
+    }
+
+    /**
+     * 注册
+     */
+    public function register()
+    {
+        $this->assign('head_title', '免费注册');
+        return $this->fetch();
+    }
+
+    /**
+     * 检验登录验证码
+     */
+    public function checkRegisterVerifyCode()
+    {
+        $this->assign('head_title', '填写校验码');
+        return $this->fetch();
+    }
+
+    /**
+     * 注册时，设置密码
+     */
+    public function setRegisterPwd()
+    {
+        $this->assign('head_title', '账户设置');
+        return $this->fetch();
+    }
+
+    /**
+     * 设置注册信息
+     */
+    public function setRegisterInfo()
+    {
+        $this->assign('head_title', '绑定房产物业');
         return $this->fetch();
     }
 
