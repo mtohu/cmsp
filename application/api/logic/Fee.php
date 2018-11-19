1fc315b65467bf6bd5bac8d17ef4ea1eaeeae13a
garyhu <garyhu@tiantu.com>
2018-11-19 17:44:50 +0800

    update

--- application/api/logic/Fee.php
+++ application/api/logic/Fee.php
@@ -1,21 +1,25 @@
 <?php
 namespace app\api\logic;
 use think\Db;
-use think\Request;
-use think\Exception;
-use think\facade\Cookie;
-use think\facade\Session;
 use errors\ErrorException;
 class Fee extends Base
 {
     /****获取自己的缴费记录列表*****/
     public function myFeeList($input){
         $resident_id = isset($input['resident_id']) ? $input['resident_id']:0;
+        $search_name = isset($input['search_name']) ? trim($input['search_name']):"";
         $page_index= isset($input['page_index'])?intval($input['page_index']):1;
         $page_index = $page_index<=0?1:$page_index;
         $page_size=isset($input['page_size'])?intval($input['page_size']):10;
         $page_start=$page_size * ($page_index-1);
         $nowdate = date('Y-m-d H:i:s',now_time());
+        $where =array();
+        $where[]=['rr.resident_id','=',$resident_id];
+        $where[]=['rr.is_verified','=',1];
+        $where[]=['rr.resident_type','in',[1,2]];
+        if(!empty($search_name)){
+            $where[]=['fi.name','like',"%".$search_name."%"];
+        }
         $field  ="f.fee_description,f.id,f.fee_amount,f.start_date,f.end_date,f.create_date,f.payment_status,
                    f.payment_date,f.fee_item_id_id,f.room_id,fi.name,fi.cycle";
         $field .= " ,(CASE 
@@ -33,8 +37,8 @@ class Fee extends Base
                    ->field($field)
                    ->leftJoin("cmp_fee_item fi","f.fee_item_id_id = fi.id")
                    ->leftJoin("cmp_resident_room rr","rr.room_id = f.room_id ")
-                   ->where([['rr.resident_id','=',$resident_id],['rr.is_verified','=',1],['rr.resident_type','in',[1,2]]])
-                   ->order("sort_orders","desc")
+                   ->where($where)
+                   ->order("sort_orders desc,id desc")
                    ->limit($page_start,$page_size)->select();
         foreach ($feeList as $key=> &$item){
             $fanzhu = Db::name("cmp_resident_room")->alias('crr')
